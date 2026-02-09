485022aa7f2c8185f7c78b087a9afc654cef3546
Marianagmn <marianagm1123@gmail.com>
2026-02-08 19:41:41 -0500

    BD Conexiones

--- MVC/modelo/ConexionDB.java
+++ MVC/modelo/ConexionDB.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/articulos_fintech?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USUARIO = "root"; // o tu usuario
-    private static final String PASSWORD = ""; // si tiene
+    private static final String USUARIO = "root"; 
+    private static final String PASSWORD = ""; 
 
     private static Connection conexion = null;
 
-    private ConexionDB() {} // evitar instancias
+    private ConexionDB() {}
 
     public static Connection getConexion() throws SQLException {
         if (conexion == null || conexion.isClosed()) {
